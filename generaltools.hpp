c4ca23624dfac0170bc057577ccb09cee7ca647d
BuildTools <unconfigured@null.spigotmc.org>
2022-08-17 17:12:19 -0500

    Started writing code for general data structures

---
+++ generaltools.hpp
@@ -0,0 +1,134 @@
+#ifndef GENERAL_TOOLS_H
+#define GENERAL_TOOLS_H
+
+#include <vector>
+
+namespace Tools
+{
+	template<typename D> 
+
+	class Node
+	{
+	public:
+		Node(D item) { data = item; }
+		D data;
+		Node<D>* next;
+	};
+
+
+	template<typename D> 
+
+	class LinkedList
+	{
+	private:
+		size_t list_size;
+	public:
+		LinkedList() {}
+		~LinkedList();
+		Node<D>* head;
+		Node<D>* tail;
+		inline size_t get_size();
+		inline void push(D);
+		void pop();
+		virtual void remove(D) = 0;
+	};
+
+
+	template<typename K, typename V>
+
+	class HashNode
+	{
+	public:
+		V value;
+		K key;
+
+		HashNode(K, V);
+	};
+
+
+	template<typename K, typename V>
+
+	class HashMap
+	{
+		HashNode<K, V>** arr;
+		int capacity;
+		int size;
+
+	public:
+		HashMap();
+		int hash_code(K);
+		void insert_node(K, V);
+		V delete_node(int);
+		V get(int);
+		int size_of_map();
+		bool is_empty();
+	};
+}
+
+template<typename D> Tools::LinkedList<D>::~LinkedList()
+{
+	if (head != NULL)
+	{
+		Node<D>* traverse = head;
+		Node<D>* traverse_ahead = head->next;
+		while (traverse != NULL)
+		{
+			delete traverse;
+			traverse = traverse_ahead;
+			if (traverse_ahead != NULL)
+			{
+				traverse_ahead = traverse_ahead->next;
+			}
+		}
+	}
+}
+
+template<typename D> inline size_t Tools::LinkedList<D>::get_size() { return list_size; }
+
+template<typename D> inline void Tools::LinkedList<D>::push(D item)
+{
+	Tools::Node<D>* new_node = new Tools::Node<D>(item);
+	if (head == NULL)
+	{
+		head = new_node;
+	}
+	else
+	{
+		if (head->next == NULL)
+		{
+			head->next = new_node;
+		}
+		else
+		{
+			tail->next = new_node;
+		}
+	}
+
+	tail = new_node;
+	list_size++;
+}
+
+template<typename D> void Tools::LinkedList<D>::pop()
+{
+	Tools::Node<D>* traverse = head;
+	Tools::Node<D>* prev = NULL;
+
+	while (traverse->next != NULL)
+	{
+		prev = traverse;
+		traverse = traverse->next;
+	}
+	delete traverse;
+	if (prev != NULL)
+	{
+		tail = prev;
+	}
+	else
+	{
+		tail = NULL;
+	}
+	list_size--;
+}
+
+
+#endif
